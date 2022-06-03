f8cea7d8c5bbb236e401232f4188e2a6ea477d39
mahri09 <mahriaa88@gmail.com>
2022-06-03 16:00:41 +0100

    tring to fix the problem

--- routes/messages.js
+++ routes/messages.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const messagesData = require("../data");
+const messagesData = require("../data.js");
 
 router.get("/", function (request, response) {
   response.json(messagesData);
